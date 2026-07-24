e2fe2630325b675ca0e7fdb0ccf491cbde0591e9
ElnazTayebi <elnazt67@gmail.com>
2026-07-24 15:49:56 +0200

    feat: add refresh token interceptor to axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -9,7 +9,7 @@ export const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = Cookies.get("token");
+  const token = Cookies.get("accessToken") || Cookies.get("token");
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -17,3 +17,46 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshToken = Cookies.get("refreshToken");
+
+        if (!refreshToken) {
+          throw new Error("No refresh token found");
+        }
+
+        const res = await axios.post("https://dummyjson.com/auth/refresh", {
+          refreshToken,
+          expiresInMins: 30,
+        });
+
+        const newAccessToken = res.data.accessToken;
+        const newRefreshToken = res.data.refreshToken;
+
+        Cookies.set("accessToken", newAccessToken);
+        Cookies.set("refreshToken", newRefreshToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return api(originalRequest);
+      } catch (refreshError) {
+        Cookies.remove("accessToken");
+        Cookies.remove("token");
+        Cookies.remove("refreshToken");
+
+        if (typeof window !== "undefined") {
+          window.location.href = "/login";
+        }
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
